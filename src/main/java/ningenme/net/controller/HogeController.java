b91aa7b18ac1868b196544fa9055c6fe70e65aab
kuru <amohkuru@gmail.com>
2020-05-09 21:15:31 +0900

    first commit

---
+++ src/main/java/ningenme/net/controller/HogeController.java
@@ -0,0 +1,12 @@
+package ningenme.net.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HogeController {
+
+    @GetMapping("/hoge")
+    public String text1() {
+        return "text content";
+    }
+}
