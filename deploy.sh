a7d16df57db8b0f46a18171b3e2e40ec198a0063
kuru <amohkuru@gmail.com>
2020-05-12 01:01:22 +0900

    modify

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ pid=`jps | awk -F: '/ningenme-net-back.jar/{print $1}' | awk '{print $1}'`
 if [ $pid != "" ] ; then
     kill -9 $pid
 fi
-java -jar ./target/ningenme-net-back.jar &
+nohup java -jar ./target/ningenme-net-back.jar &
 exit
