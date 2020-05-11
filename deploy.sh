eb38d80a069ee3c34f1bfa021ba882ff9ad2c903
kuru <amohkuru@gmail.com>
2020-05-11 23:35:52 +0900

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 ./mvnw package
+sleep 30
 pid=`lsof -i:8080 | awk -F: '/java/{print $1}' | awk '{print $2}'`
 if [ $pid != "" ] ; then
     kill -9 $pid
 fi
-java -jar ./target/ningenme-net-back.jar 
+java -jar ./target/ningenme-net-back.jar &
