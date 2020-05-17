65273f55a91e4f0833439134c9e7d0e6b83dd3d5
ningenMe <amohkuru@gmail.com>
2020-05-18 05:20:54 +0900

    modify env value

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ pid=`jps | awk -F: '/ningenme-net-back.jar/{print $1}' | awk '{print $1}'`
 if [ $pid != "" ] ; then
     kill -9 $pid
 fi
-java -Dspring.profiles.active=production -jar target/ningenme-net-back.jar &
+java -jar target/ningenme-net-back.jar &
