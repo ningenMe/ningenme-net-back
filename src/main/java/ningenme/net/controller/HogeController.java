23513151951ca82e39c5ef377590040567374fdb
ningenMe <amohkuru@gmail.com>
2020-05-19 01:39:29 +0900

    modify yml

--- src/main/java/ningenme/net/controller/HogeController.java
+++ src/main/java/ningenme/net/controller/HogeController.java
@@ -7,6 +7,6 @@ public class HogeController {
 
     @GetMapping("/hoge")
     public String text1() {
-        return "hogeeeeeeeeeeeeeeeeeeeeeeee";
+        return "hogee!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 }
