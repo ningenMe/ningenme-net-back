64311b874cc6d97b251d07e935c2f4e3d5f7359c
ningenMe <amohkuru@gmail.com>
2020-06-11 03:10:02 +0900

    add entrypoint

--- src/main/java/ningenme/net/controller/AtcoderUserController.java
+++ src/main/java/ningenme/net/controller/AtcoderUserController.java
@@ -1,9 +1,13 @@
 package ningenme.net.controller;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ningenme.net.domain.AtcoderUserContestDomain;
 import ningenme.net.domain.AtcoderUserDomain;
 import ningenme.net.service.AtcoderUserServiceInterface;
 
@@ -20,7 +24,23 @@ public class AtcoderUserController {
      * @param atcoderId atcoderのuserId
      */
     @GetMapping(path = "/{atcoder_id}", produces = "application/json")
-    public AtcoderUserDomain get(@PathVariable("atcoder_id") String atcoderId) {
+    public AtcoderUserDomain getAtcoderUser(@PathVariable("atcoder_id") String atcoderId) {
         return atcoderUserServiceInterface.get(atcoderId);
     }
+
+    /**
+     * AtCoderのユーザーが出たコンテストの情報を参照するエンドポイント
+     * @param atcoderId atcoderのuserId
+     * @param contestType contestsのcontestType
+     */
+    @GetMapping(path = "/{atcoder_id}/contests/{contest_type}", produces = "application/json")
+    public List<AtcoderUserContestDomain> getAtcoderUserContestsContestType(
+        @PathVariable("atcoder_id") String atcoderId,
+        @PathVariable("contest_type") String contestType) {
+        List<AtcoderUserContestDomain> atcoderUserContestDomains = new ArrayList<>();
+        atcoderUserContestDomains.add(new AtcoderUserContestDomain("hoge"));
+        atcoderUserContestDomains.add(new AtcoderUserContestDomain("fuga"));
+        return atcoderUserContestDomains;
+    }
+
 }
